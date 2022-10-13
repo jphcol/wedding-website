3e4e044cb4c5df664a812da0bb0a600fad195535
jphcol <jph.coles@gmail.com>
2022-10-13 13:17:01 +0100

    fix error with js declaring twice

--- 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
@@ -1,13 +1,23 @@
-const Http = new XMLHttpRequest();
-const url = 'https://eo76a395x0bkcxi.m.pipedream.net';
-
-function handleForm () {
+function handleForm (data) {
+    const FD = new FormData(document.querySelector('form'));
+    const Http = new XMLHttpRequest();
+    const url = 'https://eo76a395x0bkcxi.m.pipedream.net';
+    
+    for (const[name, value] of Object.entries(data)) {
+        FD.append(name, value);
+    }
+    Http.addEventListener('Load', (event) => {
+        alert('Yeah! Data sent and response loaded.');
+    });
+    Http.addEventListener('error', (event) => {
+        alert('Oops! Something went wrong.');
+    });
     Http.open("POST", url);
     Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
     Http.setRequestHeader('Content-Type', 'application/json');
     Http.send(
         JSON.stringify({
-            "Name" : "Jack"
+            form
         })
     );
 
