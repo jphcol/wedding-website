2b0b59d1f1973081a25058f654fbbabea9c4f40d
Jack <104137395+jphcol@users.noreply.github.com>
2022-10-13 18:41:38 +0100

    Update formhandler.js

--- 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
@@ -7,7 +7,7 @@ Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
 Http.setRequestHeader('Content-Type', 'application/json');
 Http.send(
     JSON.stringify({
-        "Guest_Number" : "160622",
+        "Guest_Number" : 160622,
         nameForm
         })
 );
