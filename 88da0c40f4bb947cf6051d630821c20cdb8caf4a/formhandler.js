af9217b9ea81391b85bed30a10ad862399858ea7
jphcol <jph.coles@gmail.com>
2022-10-13 12:46:50 +0100

    fix error with js declaring twice

---
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
@@ -0,0 +1,17 @@
+function handleForm () {
+    const Http = new XMLHttpRequest();
+    const url = 'https://eo76a395x0bkcxi.m.pipedream.net';
+    Http.open("POST", url);
+    Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
+    Http.setRequestHeader('Content-Type', 'application/json');
+    Http.send(
+        JSON.stringify({
+            "Name" : "Jack"
+        })
+    );
+
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText);
+    };
+};
--- 88da0c40f4bb947cf6051d630821c20cdb8caf4a/index.js
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/index.js
@@ -5,21 +5,3 @@ burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active')
     burgerIcon.classList.toggle('is-active')
 });
-
-function handleForm () {
-    const Http = new XMLHttpRequest();
-    const url = 'https://eo76a395x0bkcxi.m.pipedream.net';
-    Http.open("POST", url);
-    Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
-    Http.setRequestHeader('Content-Type', 'application/json');
-    Http.send(
-        JSON.stringify({
-            "Name" : "Jack"
-        })
-    );
-
-
-    Http.onreadystatechange = (e) => {
-        console.log(Http.responseText);
-    };
-};
