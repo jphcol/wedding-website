9611ad4d7ee3a933abb4b1e1a48d303fbda15a64
Jack <104137395+jphcol@users.noreply.github.com>
2022-10-13 17:45:00 +0100

    Update formhandler.js

--- 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
@@ -1,28 +1,18 @@
-function handleForm (data) {
-    const FD = new FormData(document.querySelector('form'));
-    const Http = new XMLHttpRequest();
-    const url = 'https://eo76a395x0bkcxi.m.pipedream.net';
-    
-    for (const[name, value] of Object.entries(data)) {
-        FD.append(name, value);
-    }
-    Http.addEventListener('Load', (event) => {
-        alert('Yeah! Data sent and response loaded.');
-    });
-    Http.addEventListener('error', (event) => {
-        alert('Oops! Something went wrong.');
-    });
-    Http.open("POST", url);
-    Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
-    Http.setRequestHeader('Content-Type', 'application/json');
-    Http.send(
-        JSON.stringify({
-            form
+const nameForm = document.getElementById('#name');
+
+function handleForm (nameForm) {
+const Http = new XMLHttpRequest();
+const url='https://6sxnidgy4j.execute-api.eu-west-2.amazonaws.com/Production';
+Http.open("POST", url);
+Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
+Http.setRequestHeader('Content-Type', 'application/json');
+Http.send(
+    JSON.stringify({
+        nameForm
         })
-    );
+);
 
-
-    Http.onreadystatechange = (e) => {
-        console.log(Http.responseText);
-    };
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
 };
