9c04f28187cc47b10d5bf48f6321b9cb411f797c
jphcol <jph.coles@gmail.com>
2022-11-21 17:28:40 +0000

    update form handler and change std landing page

--- 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
@@ -1,18 +1,18 @@
 function handleForm () {
-const nameForm = document.getElementById('formName').value;
-const Http = new XMLHttpRequest();
-const url='https://6sxnidgy4j.execute-api.eu-west-2.amazonaws.com/Production';
-Http.open("POST", url);
-Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
-Http.setRequestHeader('Content-Type', 'application/json');
-Http.send(
-    JSON.stringify({
-        "Guest_Number" : 160622,
-        "Guest_Name" : nameForm
-        })
-);
-
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-}
-}
+    const nameForm = document.getElementById('formName').value;
+    const Http = new XMLHttpRequest();
+    const url='https://6sxnidgy4j.execute-api.eu-west-2.amazonaws.com/Production';
+    Http.open("POST", url);
+    Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
+    Http.setRequestHeader('Content-Type', 'application/json');
+    Http.send(
+        JSON.stringify({
+            "Guest_Number" : 160622,
+            "Guest_Name" : nameForm
+            })
+    );
+    
+    Http.onreadystatechange = (e) => {
+      console.log(Http.responseText)
+    }
+    }
