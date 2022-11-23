0b7e19885aba9d0588311cc92d3d22232fed552f
jackcolesmb <jack.coles@instapro.com>
2022-11-23 17:44:57 +0000

    Made a proper start on form and form handling

--- 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
@@ -1,5 +1,12 @@
 function handleForm () {
-    const nameForm = document.getElementById('formName').value;
+    const nameForm = document.getElementByIdAll('formName').value;
+    const rsvpAttending = document.getElementByIdAll('rsvpAttending').value;
+    const rsvpNotAttending = document.getElementByIdAll('rsvpNotAttending').value;
+    const firstCourse = document.getElementByIdAll('firstCourse').value;
+    const mainCourse = document.getElementByIdAll('mainCourse').value;
+    const dessertCourse = document.getElementByIdAll('dessertCourse').value;
+    const allergies = document.getElementByIdAll('allergies').value;
+    const message = document.getElementByIdAll('message').value;
     const Http = new XMLHttpRequest();
     const url='https://6sxnidgy4j.execute-api.eu-west-2.amazonaws.com/Production';
     Http.open("POST", url);
@@ -8,7 +15,14 @@ function handleForm () {
     Http.send(
         JSON.stringify({
             "Guest_Number" : 160622,
-            "Guest_Name" : nameForm
+            "Guest_Name" : nameForm,
+            "ATTENDING" : rsvpAttending,
+            "NOT_ATTENDING" : rsvpNotAttending,
+            "FIRST" : firstCourse,
+            "MAIN" : mainCourse,
+            "DESSERT" : dessertCourse,
+            "ALLERGIES" : allergies,
+            "MESSAGE" : message
             })
     );
     Http.onreadystatechange = (e) => {
--- 88da0c40f4bb947cf6051d630821c20cdb8caf4a/index.js
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/index.js
@@ -5,3 +5,11 @@ burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active')
     burgerIcon.classList.toggle('is-active')
 });
+
+function attendingCheck() {
+    if (document.getElementById('rsvpAttending').checked) {
+        document.getElementById('menuChoices').style.display = 'inline-block';
+    } else {
+        document.getElementById('menuChoices').style.display = 'none';
+    }
+};
