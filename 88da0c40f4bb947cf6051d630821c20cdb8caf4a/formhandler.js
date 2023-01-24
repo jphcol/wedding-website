e7d4683bd1c2dc94d8d0ccb82cc579110d0f5b75
jackcolesmb <jack.coles@instapro.com>
2023-01-24 12:44:44 +0000

    update JS handle form better shows success message

--- 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
+++ 88da0c40f4bb947cf6051d630821c20cdb8caf4a/formhandler.js
@@ -1,3 +1,13 @@
+window.onload=function(){
+const form = document.getElementById('rsvpForm');
+const thankYouMessage = document.querySelector('#thank-you-message');
+form.addEventListener("submit", evt => {
+  evt.preventDefault();
+  thankYouMessage.classList.add('show');
+  handleForm();
+  setTimeout(() => form.submit(), 2000);
+})
+}
 function handleForm () {
     const nameForm = document.getElementById('formName').value;
     const rsvpFunction = () => {
@@ -13,26 +23,29 @@ function handleForm () {
     const dessertCourse = document.getElementById('dessertCourse').value;
     const allergies = document.getElementById('allergies').value;
     const message = document.getElementById('message').value;
-    const Http = new XMLHttpRequest();
-    const url='https://6sxnidgy4j.execute-api.eu-west-2.amazonaws.com/Production';
-    Http.open("POST", url);
-    Http.setRequestHeader('x-api-key', 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana');
-    Http.setRequestHeader('Content-Type', 'application/json');
-    Http.send(
-        JSON.stringify({
-            "Guest_Number" : 160622,
-            "Guest_Name" : nameForm,
-            "RSVP" : rsvp,
-            "FIRST" : firstCourse,
-            "MAIN" : mainCourse,
-            "DESSERT" : dessertCourse,
-            "ALLERGIES" : allergies,
-            "MESSAGE" : message
-            })
-    );
-    Http.onreadystatechange = (e) => {
-      console.log(Http.responseText)
-    };
-    if(alert('Thanks ' + nameForm)){}
-    else    window.location.reload(); 
-    };
+    fetch('https://6sxnidgy4j.execute-api.eu-west-2.amazonaws.com/Production', {
+    method: 'POST',
+    headers: {
+      'x-api-key' : 'ddAqtAo0KzQilzliB0s76t1m8rtNM3c5pBAD6ana',
+      'Content-Type' : 'application/json'},
+    body: JSON.stringify({
+      "Guest_Number" : 160622,
+      "Guest_Name" : nameForm,
+      "RSVP" : rsvp,
+      "FIRST" : firstCourse,
+      "MAIN" : mainCourse,
+      "DESSERT" : dessertCourse,
+      "ALLERGIES" : allergies,
+      "MESSAGE" : message
+      })
+   }).then(response => {
+    if(response.ok){
+	    return response.json();  
+    }
+	  throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message);
+  }).then(jsonResponse => {
+    console.log(jsonResponse);
+  })
+}
